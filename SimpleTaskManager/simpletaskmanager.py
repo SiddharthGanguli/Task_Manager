a29aff9e6db838f46a111dd97128631b62df7113
Siddhartha Ganguli <siddharthaganguli0093@gmail.com>
2025-07-22 01:44:13 +0530

    Basic task manager with adding extra task if user have this feature is updated in this version

--- SimpleTaskManager/simpletaskmanager.py
+++ SimpleTaskManager/simpletaskmanager.py
@@ -10,11 +10,19 @@ def add_task(task_name):
     tasks.append(task)
 
 user_input = input("Do you have any active tasks? (yes/no): ")
-
 if user_input.lower() in ['yes', 'y']:
-    numberOfInputs = int(input("How many tasks do you want to add? "))
+    while True:
+        try:
+            numberOfInputs = int(input("How many tasks do you want to add? "))
+            if numberOfInputs <= 0:
+                print("Please enter a number greater than 0.")
+                continue
+            break 
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
+
     for i in range(numberOfInputs):
-        task_name = input(f"Enter task #{i+1} name: ")
+        task_name = input(f"Enter task ->{i+1} name: ")
         add_task(task_name)
 else:
     print("No tasks added. Thank you!")
@@ -22,3 +30,21 @@ else:
 print("\nYour Tasks:")
 for t in tasks:
     print(f"Time: {t['time']}  |  Task: {t['task']}")
+
+
+def adding_features(tasks):
+    noOftask=int(input("How many task you want to add ? "))
+    for i in range (noOftask):
+        task_name = input(f"Enter task ->{i+1} name: ")
+        add_task(task_name)
+        print(f"Congratulation your {noOftask+1} added")
+
+
+print("Operations are also available to performe")
+user_input2 = input("Would you like to perform any actions (Add, Edit, Delete, View)? Enter yes or no: ")
+
+if user_input2.lower() in ['yes','y']:
+    print("Available actions are Add, Edit, Delete, View")
+    user_input3 = input("Enter the action you want to perform: ")
+    if user_input3.lower() in ['add', 'a']:
+        adding_features(tasks)
